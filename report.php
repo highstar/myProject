a4da6740c8b1f64704970926dd4eae7243f47027
laopo <stellargao@me.com>
2017-12-02 12:28:34 +0800

    Modified on 2017-12-2 12:29

--- report.php
+++ report.php
@@ -1,9 +1,11 @@
 <html>
+
 <head>
   <title>Report from readers</title>
 </head>
 
 <body>
+
   <h2>Report from readers</h2>
 
   <?php
@@ -11,10 +13,15 @@
     $email = $_POST['email'];
     $description = $_POST['description'];
     $msg = "$name emails you,and says:$description.\n";
+    $to = "stellargao@yahoo.com";
+    $subject = "Reader's message";
+    mail($to, $subject, $msg, 'From:' . $email);
 
     echo '感谢提供留言。<br />';
     echo '你的邮箱地址是'.$email.'.<br />';
     echo '你的描述是：'.$description;
   ?>
+
 </body>
+
 </html>
